3f4f80e88e8226948815e2279af4da8b5fe3f29a
Frasia <108404961+SFrasia@users.noreply.github.com>
2024-01-16 21:53:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here!// Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+const newHeader = document.createElement("h1").element.remove();
+newHeader.innerHTML = "YOUR-NAME is the champion";
